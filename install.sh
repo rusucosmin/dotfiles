0ea5a7b64e17c35d95bbda593e81a470aaac6597
rusucosmin <cr.rusucosmin@gmail.com>
2016-07-07 09:42:27 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".atom .vim .bash_aliases .bash_profile .bashrc .gitconfig .vimrc"    # list of files/folders to symlink in homedir
+files=".atom .vim .bash_aliases .bash_profile .bashrc .gitconfig .vimrc .zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
